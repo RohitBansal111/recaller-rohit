b1f9752d17fc7287646b32415dbbb8fe685ac59a
IshaBharti <ishabharti18@gmail.com>
2022-03-22 09:24:59 +0530

    changes done in create new filter

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,91 +1,83 @@
-import React from 'react'
-import { NavLink, useLocation } from 'react-router-dom';
-import HomeIcon from '../../assets/svg-icons/home';
-import ContactIcon from '../../assets/svg-icons/contact';
-import Logo from '../../assets/images/logo.png';
-
-
+import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import HomeIcon from "../../assets/svg-icons/home";
+import ContactIcon from "../../assets/svg-icons/contact";
+import Logo from "../../assets/images/logo.png";
 
 const SideNavMenu = [
   {
-    path : "/",
-    menuIcon : <HomeIcon />,
-    menuTitle : "Dashboard"
+    path: "/",
+    menuIcon: <HomeIcon />,
+    menuTitle: "Dashboard",
   },
   {
-    path : "/contacts",
-    menuIcon : <ContactIcon />,
-    menuTitle : "Contacts"
+    path: "/contacts",
+    menuIcon: <ContactIcon />,
+    menuTitle: "Contacts",
   },
   {
-    path : "/text",
-    menuIcon : <HomeIcon />,
-    menuTitle : "Text"
+    path: "/text",
+    menuIcon: <HomeIcon />,
+    menuTitle: "Text",
   },
   {
-    path : "/voice",
-    menuIcon : <HomeIcon />,
-    menuTitle : "Voice"
+    path: "/voice",
+    menuIcon: <HomeIcon />,
+    menuTitle: "Voice",
   },
   {
-    path : "/search",
-    menuIcon : <HomeIcon />,
-    menuTitle : "Search"
+    path: "/search",
+    menuIcon: <HomeIcon />,
+    menuTitle: "Search",
+  },
+  // {
+  //   path : "/local-site",
+  //   menuIcon : <HomeIcon />,
+  //   menuTitle : "Local Site"
+  // },
+  // {
+  //   path : "/local-seo",
+  //   menuIcon : <HomeIcon />,
+  //   menuTitle : "Local SEO"
+  // },
+  // {
+  //   path : "/local-ads",
+  //   menuIcon : <HomeIcon />,
+  //   menuTitle : "Local Ads"
+  // },
+  {
+    path: "/settings",
+    menuIcon: <HomeIcon />,
+    menuTitle: "Settings",
   },
   {
-    path : "/local-site",
-    menuIcon : <HomeIcon />,
-    menuTitle : "Local Site"
+    path: "/recallr-AI",
+    menuIcon: <HomeIcon />,
+    menuTitle: "RecallrAI",
   },
-  {
-    path : "/local-seo",
-    menuIcon : <HomeIcon />,
-    menuTitle : "Local SEO"
-  },
-  {
-    path : "/local-ads",
-    menuIcon : <HomeIcon />,
-    menuTitle : "Local Ads"
-  },
-  {
-    path : "/settings",
-    menuIcon : <HomeIcon />,
-    menuTitle : "Settings"
-  },
-  {
-    path : "/recallr-AI",
-    menuIcon : <HomeIcon />,
-    menuTitle : "RecallrAI"
-  }
-]
+];
 
 const Sidebar = () => {
   const location = useLocation();
   return (
-    <div className='inner-sidebar'>
-        <div className='brand-logo'>
-            <img src={Logo} alt="Recallr"/>
-        </div>
-        <ul className='sidebar-menu'>
-            {
-              SideNavMenu.map((item, index)=>{
-                return(
-                  <li key={index}>
-                      <NavLink 
-                      className={location === location.path ? 'active' : ''}
-                      to={item.path}>
-                        <div className='media-avtar'>
-                          {item.menuIcon}
-                        </div>
-                        <span> {item.menuTitle} </span>
-                      </NavLink>
-                  </li>
-                )
-              })
-            }
-        </ul>
+    <div className="inner-sidebar">
+      <div className="brand-logo">
+        <img src={Logo} alt="Recallr" />
+      </div>
+      <ul className="sidebar-menu">
+        {SideNavMenu.map((item, index) => {
+          return (
+            <li key={index}>
+              <NavLink className={location === location.path ? "active" : ""} to={item.path}>
+                <div className="media-avtar">{item.menuIcon}</div>
+                <span> {item.menuTitle} </span>
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
