19107bc9194447ef2bc9255d4eb5993651d89a09
soniaemi <sonia.eminence@gmail.com>
2022-04-13 14:48:36 +0530

    add tags in text page

--- src/components/text/conversationTagModal.js
+++ src/components/text/conversationTagModal.js
@@ -1,50 +1,92 @@
-import React, { useState } from 'react'
-import { Modal } from 'react-responsive-modal';
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
-import LocalOfferIcon from '@material-ui/icons/LocalOffer';
-import EditTagModal from '../settings/EditTagModal';
+import React, { useState } from "react";
+import { Modal } from "react-responsive-modal";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import EditTagModal from "../settings/EditTagModal";
+import CreateTagModal from "../../components/settings/createTagModal";
+import DeleteTagsModal from "../../models/deleteTagsModal";
 
-const ConversationTagModal = ({open, handleCloseManageModal}) => {
-    const [openEditTag, setOpenEditTag] = useState(false)
-    const handleEditTag = () => { setOpenEditTag(true) }
-    const handleCloseETModal = () => {setOpenEditTag(false)}
+const ConversationTagModal = ({ open, handleCloseManageModal, ...props }) => {
   return (
-      <>
-        <Modal open={open} onClose={handleCloseManageModal} center>
-            <div className="modal-header">
-                <h3>Campaign Tags</h3>
+    <>
+      <Modal open={open} onClose={handleCloseManageModal} center>
+        <div className="modal-header">
+          <h3>Campaign Tags</h3>
+        </div>
+        <div className="modal-body">
+          <form className="main-form">
+            <div className="w-100 flexFull tagsField">
+              <ul>
+                {props.tags
+                  ? props.tags.map((item) => (
+                      <li>
+                        <span
+                          style={{ borderColor: item.color, color: item.color }}
+                        >
+                          {" "}
+                          <LocalOfferIcon
+                            style={{ fill: "green !important" }}
+                          />{" "}
+                          {item.name}
+                        </span>
+                        <div className="tags-actions">
+                          <EditIcon
+                            titleAccess="Edit"
+                            onClick={() => props.handleEditTag(item)}
+                          />
+                          <DeleteIcon
+                            titleAccess="Delete"
+                            onClick={() => props.handleDelModal(item)}
+                          />
+                        </div>
+                      </li>
+                    ))
+                  : []}
+              </ul>
             </div>
-            <div className="modal-body">
-                <form className="main-form">
-                    <div className="w-100 flexFull tagsField">
-                        <ul>
-                            <li>
-                                <span style={{borderColor: 'green', color: 'green'}}> <LocalOfferIcon style={{fill:'green !important'}} /> Good Customer</span>
-                                <div className='tags-actions'>
-                                    <EditIcon onClick={handleEditTag} />
-                                    <DeleteIcon />
-                                </div>
-                            </li>
-                            <li>
-                            <span style={{borderColor: 'blue', color: 'blue'}}> <LocalOfferIcon style={{fill:'blue !important'}} /> New Customer</span>
-                                <div className='tags-actions'>
-                                    <EditIcon onClick={handleEditTag} />
-                                    <DeleteIcon />
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="field-group flexFull text-center mt-3">
-                        <button type="button" className="btn btn-cancel me-3" onClick={() => handleCloseManageModal()} > Dismiss </button>
-                        <button type="button" className="btn btn-primary"> Create Tag </button>
-                    </div>
-                </form>
+            <div className="field-group flexFull text-center mt-3">
+              <button
+                type="button"
+                className="btn btn-cancel me-3"
+                onClick={handleCloseManageModal}
+              >
+                {" "}
+                Dismiss{" "}
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={props.handleCMModal}
+              >
+                {" "}
+                Create Tag{" "}
+              </button>
             </div>
-        </Modal>
-        <EditTagModal open={openEditTag} handleCloseETModal={handleCloseETModal} />
-      </>
-  )
-}
+          </form>
+        </div>
+      </Modal>
+      <EditTagModal
+        open={props.openEditTagModal}
+        handleCloseETModal={props.handleCloseETModal}
+        handleChange={props.handleEditChange}
+        handleEdit={props.handleEdit}
+        editTags={props.editTags}
+      />
+      <CreateTagModal
+        open={props.openCTM}
+        handleCloseCTModal={props.handleCloseCTModal}
+        addTags={props.addTags}
+        handleChange={props.handleChange}
+        handleClick={props.handleClick}
+      />
+      <DeleteTagsModal
+        showDeleteTagModal={props.showDeleteTagModal}
+        handleDeleteTagsData={props.handleDeleteTags}
+        handleCloseDeleteModal={props.handleCloseDeleteModal}
+      />
+    </>
+  );
+};
 
-export default ConversationTagModal
+export default ConversationTagModal;
