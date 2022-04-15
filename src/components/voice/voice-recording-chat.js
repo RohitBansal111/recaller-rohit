d2cf24d43c88c00d93aa436fcf3002a600989ad3
achhar-primelab <achhar.singh@primelab.io>
2022-04-15 16:07:50 +0530

    voice screen design

---
+++ src/components/voice/voice-recording-chat.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import ClientProfile from './../../assets/images/user-profile.jpeg'
+import UserProfile from './../../assets/images/user-profile2.webp'
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+
+const VoiceRecordingChat = () => {
+  return (
+    <div className='middle-chat-screen voice-chat-screen'>
+        <ul>
+            <li className='client'> 
+                <div className='avtar'>
+                    <img src={ClientProfile} alt="avtar" />
+                </div>
+                <PlayArrowIcon />
+                <div className='background-recording'></div>
+            </li>
+            <li className='user'> 
+                <div className='avtar'>
+                    <img src={UserProfile} alt="avtar" />
+                </div>
+                <PlayArrowIcon />
+                <div className='background-recording'></div>
+            </li>
+            <li className='client'> 
+                <div className='avtar'>
+                    <img src={ClientProfile} alt="avtar" />
+                </div>
+                <PlayArrowIcon />
+                <div className='background-recording'></div>
+            </li>
+            <li className='user'> 
+                <div className='avtar'>
+                    <img src={UserProfile} alt="avtar" />
+                </div>
+                <PlayArrowIcon />
+                <div className='background-recording'></div>
+            </li>
+            <li className='client'> 
+                <div className='avtar'>
+                    <img src={ClientProfile} alt="avtar" />
+                </div>
+                <PlayArrowIcon />
+                <div className='background-recording'></div>
+            </li>
+            <li className='user'> 
+                <div className='avtar'>
+                    <img src={UserProfile} alt="avtar" />
+                </div>
+                <PlayArrowIcon />
+                <div className='background-recording'></div>
+            </li>
+            <li className='client'> 
+                <div className='avtar'>
+                    <img src={ClientProfile} alt="avtar" />
+                </div>
+                <PlayArrowIcon />
+                <div className='background-recording'></div>
+            </li>
+            <li className='user'> 
+                <div className='avtar'>
+                    <img src={UserProfile} alt="avtar" />
+                </div>
+                <PlayArrowIcon />
+                <div className='background-recording'></div>
+            </li>
+            <li className='client'> 
+                <div className='avtar'>
+                    <img src={ClientProfile} alt="avtar" />
+                </div>
+                <PlayArrowIcon />
+                <div className='background-recording'></div>
+            </li>
+            <li className='user'> 
+                <div className='avtar'>
+                    <img src={UserProfile} alt="avtar" />
+                </div>
+                <PlayArrowIcon />
+                <div className='background-recording'></div>
+            </li>
+            <li className='client'> 
+                <div className='avtar'>
+                    <img src={ClientProfile} alt="avtar" />
+                </div>
+                <PlayArrowIcon />
+                <div className='background-recording'></div>
+            </li>
+            <li className='user'> 
+                <div className='avtar'>
+                    <img src={UserProfile} alt="avtar" />
+                </div>
+                <PlayArrowIcon />
+                <div className='background-recording'></div>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default VoiceRecordingChat
