9d0f9563384031cffa7be9ee85c5c307259a2492
achhar-primelab <achhar.singh@primelab.io>
2022-04-21 17:49:46 +0530

    chhat tag design

--- src/components/email/emailChatText.js
+++ src/components/email/emailChatText.js
@@ -4,15 +4,46 @@ const EmailChatText = () => {
   return (
     <ScrollToBottom className="middle-chat-screen">
         <ul>
-          <li class="user">demo chat <p><b>Dev Test</b></p></li>
-          <li class="user">testtesttesttest <p><b>Dev Test</b></p></li>
-          <li class="client">hello <p><b>Dev Test</b></p></li>
-          <li class="user">hello test <p><b>Dev Test</b></p></li>
-          <li class="user">test <p><b>Dev Test</b></p></li>
-          <li class="client">test demo <p><b>Dev Test</b></p></li>
-          <li class="user">test <p><b>Dev Test</b></p></li>
-          <li class="user">test <p><b>Dev Test</b></p></li>
-          <li class="user">test new <p><b>Dev Test</b></p></li>
+          <li>
+            <div className="chat-ui-box user"> Hello
+            </div>
+            <span><b>NH</b> 5:56 AM</span>
+          </li>
+          <li>
+            <div className="chat-ui-box client"> Hii
+            </div>
+            <span><b>NH</b> 5:56 AM</span>
+          </li>
+          <li>
+            <div className="chat-ui-box user"> Hello
+            </div>
+            <span><b>NH</b> 5:56 AM</span>
+          </li>
+          <li>
+            <div className="chat-ui-box client"> Hii
+            </div>
+            <span><b>NH</b> 5:56 AM</span>
+          </li>
+          <li>
+            <div className="chat-ui-box user"> Hello
+            </div>
+            <span><b>NH</b> 5:56 AM</span>
+          </li>
+          <li>
+            <div className="chat-ui-box client"> Hii
+            </div>
+            <span><b>NH</b> 5:56 AM</span>
+          </li>
+          <li>
+            <div className="chat-ui-box user"> Hello
+            </div>
+            <span><b>NH</b> 5:56 AM</span>
+          </li>
+          <li>
+            <div className="chat-ui-box client"> Hii
+            </div>
+            <span><b>NH</b> 5:56 AM</span>
+          </li>
         </ul>
     </ScrollToBottom>
   );
--- src/components/voice/voice-recording-chat.js
+++ src/components/voice/voice-recording-chat.js
@@ -2,97 +2,94 @@ import React from 'react'
 import ClientProfile from './../../assets/images/user-profile.jpeg'
 import UserProfile from './../../assets/images/user-profile2.webp'
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 const VoiceRecordingChat = () => {
   return (
-    <div className='middle-chat-screen voice-chat-screen'>
+    <ScrollToBottom className='middle-chat-screen voice-chat-screen'>
         <ul>
-            <li className='client'> 
-                <div className='avtar'>
-                    <img src={ClientProfile} alt="avtar" />
+            <li className='voice-chat-list'>
+                <div className="chat-ui-box client"> 
+                    <div className='avtar'>
+                        <img src={ClientProfile} alt="avtar" />
+                    </div>
+                    <PlayArrowIcon />
+                    <div className='background-recording'></div>
                 </div>
-                <PlayArrowIcon />
-                <div className='background-recording'></div>
+                <span><b>NH</b> 5:56 AM</span>
             </li>
-            <li className='user'> 
-                <div className='avtar'>
-                    <img src={UserProfile} alt="avtar" />
+            <li className='voice-chat-list'>
+                <div className="chat-ui-box user"> 
+                    <div className='avtar'>
+                        <img src={UserProfile} alt="avtar" />
+                    </div>
+                    <PlayArrowIcon />
+                    <div className='background-recording'></div>
                 </div>
-                <PlayArrowIcon />
-                <div className='background-recording'></div>
+                <span><b>NH</b> 5:56 AM</span>
             </li>
-            <li className='client'> 
-                <div className='avtar'>
-                    <img src={ClientProfile} alt="avtar" />
+            <li className='voice-chat-list'>
+                <div className="chat-ui-box client"> 
+                    <div className='avtar'>
+                        <img src={ClientProfile} alt="avtar" />
+                    </div>
+                    <PlayArrowIcon />
+                    <div className='background-recording'></div>
                 </div>
-                <PlayArrowIcon />
-                <div className='background-recording'></div>
+                <span><b>NH</b> 5:56 AM</span>
             </li>
-            <li className='user'> 
-                <div className='avtar'>
-                    <img src={UserProfile} alt="avtar" />
+            <li className='voice-chat-list'>
+                <div className="chat-ui-box user"> 
+                    <div className='avtar'>
+                        <img src={UserProfile} alt="avtar" />
+                    </div>
+                    <PlayArrowIcon />
+                    <div className='background-recording'></div>
                 </div>
-                <PlayArrowIcon />
-                <div className='background-recording'></div>
+                <span><b>NH</b> 5:56 AM</span>
             </li>
-            <li className='client'> 
-                <div className='avtar'>
-                    <img src={ClientProfile} alt="avtar" />
+            <li className='voice-chat-list'>
+                <div className="chat-ui-box client"> 
+                    <div className='avtar'>
+                        <img src={ClientProfile} alt="avtar" />
+                    </div>
+                    <PlayArrowIcon />
+                    <div className='background-recording'></div>
                 </div>
-                <PlayArrowIcon />
-                <div className='background-recording'></div>
+                <span><b>NH</b> 5:56 AM</span>
             </li>
-            <li className='user'> 
-                <div className='avtar'>
-                    <img src={UserProfile} alt="avtar" />
+            <li className='voice-chat-list'>
+                <div className="chat-ui-box user"> 
+                    <div className='avtar'>
+                        <img src={UserProfile} alt="avtar" />
+                    </div>
+                    <PlayArrowIcon />
+                    <div className='background-recording'></div>
                 </div>
-                <PlayArrowIcon />
-                <div className='background-recording'></div>
+                <span><b>NH</b> 5:56 AM</span>
             </li>
-            <li className='client'> 
-                <div className='avtar'>
-                    <img src={ClientProfile} alt="avtar" />
+            <li className='voice-chat-list'>
+                <div className="chat-ui-box client"> 
+                    <div className='avtar'>
+                        <img src={ClientProfile} alt="avtar" />
+                    </div>
+                    <PlayArrowIcon />
+                    <div className='background-recording'></div>
                 </div>
-                <PlayArrowIcon />
-                <div className='background-recording'></div>
+                <span><b>NH</b> 5:56 AM</span>
             </li>
-            <li className='user'> 
-                <div className='avtar'>
-                    <img src={UserProfile} alt="avtar" />
+            <li className='voice-chat-list'>
+                <div className="chat-ui-box user"> 
+                    <div className='avtar'>
+                        <img src={UserProfile} alt="avtar" />
+                    </div>
+                    <PlayArrowIcon />
+                    <div className='background-recording'></div>
                 </div>
-                <PlayArrowIcon />
-                <div className='background-recording'></div>
-            </li>
-            <li className='client'> 
-                <div className='avtar'>
-                    <img src={ClientProfile} alt="avtar" />
-                </div>
-                <PlayArrowIcon />
-                <div className='background-recording'></div>
-            </li>
-            <li className='user'> 
-                <div className='avtar'>
-                    <img src={UserProfile} alt="avtar" />
-                </div>
-                <PlayArrowIcon />
-                <div className='background-recording'></div>
-            </li>
-            <li className='client'> 
-                <div className='avtar'>
-                    <img src={ClientProfile} alt="avtar" />
-                </div>
-                <PlayArrowIcon />
-                <div className='background-recording'></div>
-            </li>
-            <li className='user'> 
-                <div className='avtar'>
-                    <img src={UserProfile} alt="avtar" />
-                </div>
-                <PlayArrowIcon />
-                <div className='background-recording'></div>
+                <span><b>NH</b> 5:56 AM</span>
             </li>
         </ul>
-    </div>
+    </ScrollToBottom>
   )
 }
 
