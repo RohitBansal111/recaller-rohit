05d015569add5f0c973e333350b63efed553635e
shalin <shalin.eminence@gmail.com>
2022-08-29 14:24:03 +0530

    fix chart issue

--- src/components/text/chatBoot.js
+++ src/components/text/chatBoot.js
@@ -176,44 +176,33 @@ const ChatBoot = (props) => {
   };
 
   var options = {
-    series: [
-      {
-        name: "series1",
-        data: [31, 40, 28, 51, 42, 109, 100],
-      },
-      {
-        name: "series2",
-        data: [11, 32, 45, 32, 34, 52, 41],
-      },
-    ],
     chart: {
       height: 350,
-      type: "area",
+      type: 'area'
     },
     dataLabels: {
-      enabled: false,
+      enabled: false
     },
     stroke: {
-      curve: "smooth",
+      curve: 'smooth'
     },
     xaxis: {
-      type: "datetime",
-      categories: [
-        "2018-09-19T00:00:00.000Z",
-        "2018-09-19T01:30:00.000Z",
-        "2018-09-19T02:30:00.000Z",
-        "2018-09-19T03:30:00.000Z",
-        "2018-09-19T04:30:00.000Z",
-        "2018-09-19T05:30:00.000Z",
-        "2018-09-19T06:30:00.000Z",
-      ],
+      type: 'datetime',
+      categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
     },
     tooltip: {
       x: {
-        format: "dd/MM/yy HH:mm",
+        format: 'dd/MM/yy HH:mm'
       },
     },
   };
+  var series = [{
+    name: 'series1',
+    data: [31, 40, 28, 51, 42, 109, 100]
+  }, {
+    name: 'series2',
+    data: [11, 32, 45, 32, 34, 52, 41]
+  }];
 
   return (
     <div className="chatbox-warpper">
@@ -763,7 +752,7 @@ const ChatBoot = (props) => {
           <div className="monthly-credit-use">
             <h1>Monthly Credit usage</h1>
             <div className="monthly-graph">
-                <Chart options={options} />
+                <Chart options={options} series={series} type="area" />
             </div>
           </div>
           {/* Monthly credit usage column end */}
