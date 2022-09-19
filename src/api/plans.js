f51b08056a1fe5c98d50471ee52df5585e8df853
RohitBansal111 <rohitbansal.eminence@gmail.com>
2022-09-19 12:16:11 +0530

    new fkkfjf

--- src/api/plans.js
+++ src/api/plans.js
@@ -18,4 +18,35 @@ const CreateSubscription = async (data) => {
   }
 };
 
-export { CreateSubscription };
+const CreateSubscriptionFree = async () => {
+  try {
+    const AUTH_TOKEN = Cookies.get('token')
+    console.log(AUTH_TOKEN)
+    axios.defaults.headers.common["Authorization"] = AUTH_TOKEN;
+    const result = await axios.get(
+      `${process.env.REACT_APP_API_URL}/payment/add-freesub`,
+    );
+    if (result) {
+      return result;
+    }
+  } catch (err) {
+    return { data: err.response.data };
+  }
+};
+
+const GetSubscriptionData=async()=>{
+  try {
+    const AUTH_TOKEN = localStorage.getItem('token')
+    axios.defaults.headers.common["Authorization"] = AUTH_TOKEN;
+    const result = await axios.get(
+      `${process.env.REACT_APP_API_URL}/payment/sub-data`,
+    );
+    if (result) {
+      return result;
+    }
+  } catch (err) {
+    return { data: err.response.data };
+  }
+}
+
+export { CreateSubscription,CreateSubscriptionFree ,GetSubscriptionData };
